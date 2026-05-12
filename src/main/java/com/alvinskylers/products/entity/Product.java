325cd41cf0de3dd246f0ca7ee030ea11d5939779
alvinskylers <alvinskylers.dev@gmail.com>
2026-05-12 22:55:10 +0800

    add: product entity

---
+++ src/main/java/com/alvinskylers/products/entity/Product.java
@@ -0,0 +1,45 @@
+package com.alvinskylers.products.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Builder
+@Table(name="products")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    private String sku;
+
+    private String slug;
+
+    private String description;
+
+    private double price;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_category_id")
+    private ProductCategory productCategory;
+
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
