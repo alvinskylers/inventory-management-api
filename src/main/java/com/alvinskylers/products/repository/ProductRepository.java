df8b8a2e6592282f430321d01771465a266d8064
alvinskylers <alvinskylers.dev@gmail.com>
2026-05-13 10:36:07 +0800

    add: query methods in product repository

--- src/main/java/com/alvinskylers/products/repository/ProductRepository.java
+++ src/main/java/com/alvinskylers/products/repository/ProductRepository.java
@@ -1,10 +1,35 @@
 package com.alvinskylers.products.repository;
 
 import com.alvinskylers.products.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    @Query("SELECT p FROM Product p " +
+            "WHERE LOWER(p.name) " +
+            "LIKE LOWER(CONCAT('%', :query, '%'))")
+    Page<Product> findProductByName(@Param("query") String query, Pageable pageable);
+
+
+    @Query("SELECT p FROM Product p " +
+            "WHERE LOWER(p.sku) " +
+            "LIKE LOWER(CONCAT('%', :query, '%'))")
+    Page<Product> findProductBySKU(@Param("query") String query, Pageable pageable);
+
+
+    @Query("SELECT p FROM Product p " +
+            "WHERE p.price " +
+            "BETWEEN :minPrice AND :maxPrice")
+    Page<Product> filterProductByPrice(
+            @Param("minPrice") BigDecimal minPrice,
+            @Param("maxPrice") BigDecimal maxPrice,
+            Pageable pageable);
 }
