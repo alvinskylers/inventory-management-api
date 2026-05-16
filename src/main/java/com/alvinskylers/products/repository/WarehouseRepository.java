67cabfe505ba23cf9f6ce720e57ee01869a4ceec
alvinskylers <alvinskylers.dev@gmail.com>
2026-05-16 15:35:35 +0800

    add: warehouse repository

---
+++ src/main/java/com/alvinskylers/products/repository/WarehouseRepository.java
@@ -0,0 +1,9 @@
+package com.alvinskylers.products.repository;
+
+import com.alvinskylers.products.entity.Warehouse;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WarehouseRepository extends JpaRepository<Warehouse, Long> {
+}
