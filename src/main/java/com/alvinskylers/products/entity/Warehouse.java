6843bb7a4a0ac0a68c3a2fa8d90072ae6c13128e
alvinskylers <alvinskylers.dev@gmail.com>
2026-05-16 15:21:22 +0800

    add: warehouse entity

---
+++ src/main/java/com/alvinskylers/products/entity/Warehouse.java
@@ -0,0 +1,37 @@
+package com.alvinskylers.products.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Builder
+@Table(name = "warehouses")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Warehouse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+}
